775857191a72e2fc7aaac12624ec6e0322460e74
zerow41 <dudtj0410@naver.com>
2021-10-11 19:48:59 +0900

    2021-10-11

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
-import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponent';
+//import SassComponent from './SassComponent';
+//import CSSModule from './CSSModule';
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        <StyledComponent />
       </div>
     );
   }
